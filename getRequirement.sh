b27cd8a4b0922cde1c90b62b908f826642d80170
brian <brian22e3@hotmail.com>
2018-12-01 14:24:02 -0800

    correct unzip

--- getRequirement.sh
+++ getRequirement.sh
@@ -3,7 +3,7 @@ python download_glue_data.py
 wget http://nlp.stanford.edu/data/glove.6B.zip
 mv glove.6B.zip glove
 cd glove
-unzip gitglove.6B.zip
+unzip glove.6B.zip
 cd ..
 
 pip install allennlp
