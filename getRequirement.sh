cc349f2e8156f3fad6f8d47e491e480683b1bb96
brian <brian22e3@hotmail.com>
2018-12-01 14:12:48 -0800

    update

--- getRequirement.sh
+++ getRequirement.sh
@@ -3,7 +3,7 @@ python download_glue_data.py
 wget http://nlp.stanford.edu/data/glove.6B.zip
 mv glove.6B.zip glove
 cd glove
-unzip glove/glove.6B.zip
+unzip gitglove.6B.zip
 cd ..
 
 pip install allennlp
