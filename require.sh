bc36f998090f1a75bc541441e7388e480e6e3ab1
Apple <rex9999772001@gmail.com>
2018-12-03 21:00:57 -0800

    Make this code runnable on colaboratory

---
+++ require.sh
@@ -0,0 +1,18 @@
+
+python download_glue_data.py
+cd glove
+mkdir glove.6B
+cd  glove.6B
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
+cd ..
+cd ..
+
+pip install ipdb
+pip install torch==0.3.1
+pip install allennlp==0.4.3
+pip install tensorboardX
+
+
+cd src/
+python download_nltk_punk.py
---
+++ runOnCollabCmd.sh
@@ -0,0 +1,9 @@
+!git clone https://github.com/brian2213/CS273A/tree/rex/dev
+
+%cd CS273A/
+
+!sh getRequirement.sh
+
+%cd src/
+
+!sh run_stuff_GPU.sh
